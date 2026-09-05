9782133aa61fb4c2c78a5ce9c81515935558a10b
Margarithuang <2663956433@qq.com>
2026-09-05 22:39:54 +0800

    Initial commit

---
+++ android/src/main/java/com/example/goldminer/android/AndroidLauncher.java
@@ -0,0 +1,19 @@
+package com.example.goldminer.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import vn.edu.tdt.finalproject.Application;
+
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Application(), configuration);
+    }
+}
---
+++ core/src/main/java/vn/edu/tdt/finalproject/utils/SplashDoors.java
@@ -0,0 +1,93 @@
+package vn.edu.tdt.finalproject.utils;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+
+import vn.edu.tdt.finalproject.Application;
+
+public class SplashDoors {
+    private static Texture splashDoorLeft;
+    private static Texture splashDoorRight;
+    private static float[] doorDirectionX;
+    private static TextNoBackground loadingText;
+
+    public static void loadResources(){
+         splashDoorLeft= new Texture("images/backgrounds/splashdoor.png");
+         splashDoorRight = new Texture("images/backgrounds/splashdoor.png");
+
+         float screenHalfWidth = Gdx.graphics.getWidth() /2f;
+         doorDirectionX = new float[]{0, screenHalfWidth};
+
+         loadingText = new TextNoBackground(Color.GOLD, 30);
+    }
+
+    public static void setDoorOpen(){
+        doorDirectionX[0] = (-1.0f)*Application.DESKTOP_WIDTH/2;
+        doorDirectionX[1] = Application.DESKTOP_WIDTH;
+    }
+
+    public static void setDoorClose(){
+        doorDirectionX[0] = 0;
+        doorDirectionX[1] = Application.DESKTOP_WIDTH/2;
+    }
+
+    public static boolean checkDoorClose(){
+        if((doorDirectionX[0] == 0) && (doorDirectionX[1] == Application.DESKTOP_WIDTH/2)){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean checkDoorOpen(){
+        if((doorDirectionX[0] == (-1.0f)*Application.DESKTOP_WIDTH/2) && (doorDirectionX[1] == Application.DESKTOP_WIDTH)){
+            return true;
+        }
+        return false;
+    }
+
+    public static void openTheSplashDoor(float moveSpeed){
+        if(doorDirectionX[0] > (-1.0f)*Application.DESKTOP_WIDTH/2){
+            doorDirectionX[0] -= moveSpeed*ScreenConstants.TRANSFORM_X;
+        }
+        if(doorDirectionX[1] <= Application.DESKTOP_WIDTH){
+            doorDirectionX[1] += moveSpeed*ScreenConstants.TRANSFORM_X;
+        }
+        if(doorDirectionX[0] < (-1.0f)*Application.DESKTOP_WIDTH/2){
+            doorDirectionX[0] = (-1.0f)*Application.DESKTOP_WIDTH/2;
+        }
+        if(doorDirectionX[1] > Application.DESKTOP_WIDTH){
+            doorDirectionX[1] = Application.DESKTOP_WIDTH;
+        }
+    }
+
+    public static void closeTheSplashDoor(float moveSpeed){
+        if(doorDirectionX[0] < 0){
+            doorDirectionX[0] += moveSpeed*ScreenConstants.TRANSFORM_X;
+        }
+        if(doorDirectionX[1] > Application.DESKTOP_WIDTH/2){
+            doorDirectionX[1] -= moveSpeed*ScreenConstants.TRANSFORM_X;
+        }
+        if(doorDirectionX[0] > 0){
+            doorDirectionX[0] = 0;
+        }
+        if(doorDirectionX[1] < Application.DESKTOP_WIDTH/2){
+            doorDirectionX[1] = Application.DESKTOP_WIDTH/2;
+        }
+    }
+
+    public static void drawDoor(Batch batch){
+        batch.draw(splashDoorLeft, doorDirectionX[0], 0f, Application.DESKTOP_WIDTH/2, Application.DESKTOP_HEIGHT);
+        batch.draw(splashDoorRight, doorDirectionX[1], 0f, Application.DESKTOP_WIDTH/2, Application.DESKTOP_HEIGHT);
+        if(checkDoorClose()){
+            loadingText.drawText(batch, "Loading...", Application.DESKTOP_WIDTH/2 - 80f, Application.DESKTOP_HEIGHT/2 + 10f);
+        }
+    }
+
+    public static void dispose(){
+        splashDoorLeft.dispose();
+        splashDoorRight.dispose();
+        loadingText.dispose();
+    }
+}
